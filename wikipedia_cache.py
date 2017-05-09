9e8eca738772f8f598c32348ca2b27ce6c7fee92
Shai Ohev Zion <shai.oz@gmail.com>
2017-05-09 18:15:18 +0300

    if page already exists in db, fetch page from db instead

--- wikipedia_cache.py
+++ wikipedia_cache.py
@@ -9,20 +9,31 @@ def index():
     return "hello world"
 
 
-def check_if_page_cached(page_requested):
+def get_cache_if_exists(page_requested):
+    conn = pymysql.Connect(host="localhost", user="root", password="shai7588", db="wikipedia-cache")
+    cur = conn.cursor()
+    cur.execute("SELECT data from pages where name = %s", (page_requested))
+    res = cur.fetchone()
+    if res:
+        return res
     return False
+
 
 
 @app.route('/<page_requested>')
 def cache(page_requested):
-    is_cached = check_if_page_cached(page_requested)
-    if not is_cached:
+    cache = get_cache_if_exists(page_requested)
+    if not cache:
         response = requests.get("https://en.wikipedia.org/wiki/%s" % page_requested)
         response_html = response.content
         response_html = response_html.decode("utf-8")
         new_html = response_html.replace('href="/w/', 'href="https://en.wikipedia.org/w/')
         new_html = new_html.replace('src="/w/', 'src="https://en.wikipedia.org/w/')
+        print("returning %s from wikipedia..." % page_requested)
         return new_html
+    else:
+        print("returning %s from db cache..." % page_requested)
+        return cache
 
 
 if __name__ == '__main__':
