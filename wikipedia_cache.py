35c5b680b4f25d2c9581bfb4cfdcb1e27d8fa950
Shai Ohev Zion <shai.oz@gmail.com>
2017-05-09 17:49:00 +0300

    added dummy check

--- wikipedia_cache.py
+++ wikipedia_cache.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pymysql
 import requests
 
 app = Flask(__name__)
@@ -7,15 +8,22 @@ app = Flask(__name__)
 def index():
     return "hello world"
 
+
+def check_if_page_cached(page_requested):
+    return False
+
+
 @app.route('/<page_requested>')
 def cache(page_requested):
-    response = requests.get("https://en.wikipedia.org/wiki/%s" % page_requested)
-    response_html = response.content
-    response_html = response_html.decode("utf-8")
-    new_html = response_html.replace('href="/w/', 'href="https://en.wikipedia.org/w/')
-    new_html = new_html.replace('src="/w/', 'src="https://en.wikipedia.org/w/')
+    is_cached = check_if_page_cached(page_requested)
+    if not is_cached:
+        response = requests.get("https://en.wikipedia.org/wiki/%s" % page_requested)
+        response_html = response.content
+        response_html = response_html.decode("utf-8")
+        new_html = response_html.replace('href="/w/', 'href="https://en.wikipedia.org/w/')
+        new_html = new_html.replace('src="/w/', 'src="https://en.wikipedia.org/w/')
+        return new_html
 
-    return new_html
 
 if __name__ == '__main__':
     app.run()
